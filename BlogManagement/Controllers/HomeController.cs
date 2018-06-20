d34715f970eee0fa9fe271b54f98512fc0e4ff7d
AnhAnh18 <ducanh059@gmail.com>
2018-06-20 11:04:05 +0700

    create post

--- BlogManagement/Controllers/HomeController.cs
+++ BlogManagement/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlogManagement.BLL;
+using BlogManagement.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,9 @@ namespace BlogManagement.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            AccountBLL account = new AccountBLL(new DAL.UnitOfWork.UnitOfWork(new BlogDBContext()));
+            PostBLL post = new PostBLL(new DAL.UnitOfWork.UnitOfWork(new BlogDBContext()));
+            ViewBag.lst = post.getAll();
             return View();
         }
 
@@ -18,5 +23,30 @@ namespace BlogManagement.Controllers
         {
             return PartialView();
         }
+        public ActionResult NavLeftPartial()
+        {
+            CategoryBLL category = new CategoryBLL(new DAL.UnitOfWork.UnitOfWork(new DAL.Entities.BlogDBContext()));
+            ViewBag.lstCategory = category.getAll();
+            return PartialView();
+        }
+        public ActionResult CreatePostPartial()
+        {
+            ViewBag.CategoryId = new SelectList(new BlogDBContext().Categories, "CategoryId", "Name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreatePostPartial(Post model)
+        {
+            AccountBLL account = new AccountBLL(new DAL.UnitOfWork.UnitOfWork(new BlogDBContext()));
+            PostBLL post = new PostBLL(new DAL.UnitOfWork.UnitOfWork(new BlogDBContext()));
+            model.AccountId = account.getByEmail(User.Identity.Name).AccountId;
+            model.CategoryId = 1;
+            model.DatePost = DateTime.Now;
+            post.Add(model);
+            //var strContent = form["txtContent"].ToString();//FormCollection form
+            return Redirect("Index");
+        }
+
+
     }
 }
